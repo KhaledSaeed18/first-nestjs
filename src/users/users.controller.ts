b49ebf24d2002ea153cce3ff17f54f0bd3bc0a87
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-02-01 10:42:17 +0200

    feat: add UserDTO type and integrate it into UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { UserDTO } from './dto/user.dto';
 
 @Controller('users')
 export class UsersController {
+    private users: UserDTO[] = [
+        { id: 1, name: 'John Doe', email: 'john.doe@example.com', age: 30 },
+        { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com', age: 25 },
+    ];
+
     @Get() // @Get decorator to handle GET requests
     getUsers() {
-        return [
-            { id: 1, name: 'John Doe' },
-            { id: 2, name: 'Jane Smith' },
-        ];
+        return this.users.map(({ id, name }) => ({ id, name }));
     }
 
     @Post() // @Post decorator to handle POST requests
