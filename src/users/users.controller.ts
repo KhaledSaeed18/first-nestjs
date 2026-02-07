eb146f52b047f50a3047ff9de4954d7e788bd2e1
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-02-07 09:47:18 +0200

    feat: refactor UsersController and UsersService to remove in-memory user storage and utilize service methods for user operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,69 +3,41 @@ import {
     Controller,
     Delete,
     Get,
-    NotFoundException,
     Param,
     ParseIntPipe,
     Patch,
     Post,
     Query,
 } from '@nestjs/common';
-import { CreateUserDto, UpdateUserDto, User } from './dto/user.dto';
+import { CreateUserDto, UpdateUserDto } from './dto/user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    private users: User[] = [
-        { id: 1, name: 'Mohammad', email: 'test@gmail.com', age: 28 },
-        { id: 2, name: 'Ahmad', email: 'test@gmail.com', age: 33 },
-    ];
+    constructor(private readonly usersService: UsersService) {}
 
     @Get()
     getAll(@Query('name') name: string) {
-        if (name) {
-            return this.users.filter((user) => user.name == name);
-        }
-        return this.users;
+        return this.usersService.getAll(name);
     }
 
     @Get(':id')
     findOne(@Param('id', ParseIntPipe) id: number) {
-        const user: User | undefined = this.users.find((user) => user.id == id);
-        if (user) {
-            return user;
-        }
-        throw new NotFoundException();
+        return this.usersService.findOne(id);
     }
 
     @Post()
     create(@Body() body: CreateUserDto) {
-        const newUser = {
-            id: this.users[this.users.length - 1].id + 1,
-            name: body.name,
-            email: body.email,
-            age: body.age,
-        };
-        this.users.push(newUser);
-        return newUser;
+        return this.usersService.create(body);
     }
 
     @Patch(':id')
     update(@Param('id') id: string, @Body() body: UpdateUserDto) {
-        const userIndex = this.users.findIndex((user) => user.id == +id);
-        if (userIndex == -1) {
-            throw new NotFoundException();
-        }
-        this.users[userIndex].name = body.name || this.users[userIndex].name;
-        this.users[userIndex].email = body.email || this.users[userIndex].email;
-        this.users[userIndex].age = body.age || this.users[userIndex].age;
-        return this.users[userIndex];
+        return this.usersService.update(id, body);
     }
 
     @Delete(':id')
     delete(@Param('id') id: string) {
-        const userIndex = this.users.findIndex((user) => user.id == +id);
-        if (userIndex == -1) {
-            throw new NotFoundException();
-        }
-        return this.users.splice(userIndex, 1);
+        return this.usersService.delete(id);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto, User } from './dto/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: User[] = [
+        { id: 1, name: 'Mohammad', email: 'test@gmail.com', age: 28 },
+        { id: 2, name: 'Ahmad', email: 'test@gmail.com', age: 33 },
+    ];
+
+    getAll(name?: string) {
+        if (name) {
+            return this.users.filter((user) => user.name == name);
+        }
+        return this.users;
+    }
+
+    findOne(id: number) {
+        const user: User | undefined = this.users.find((user) => user.id == id);
+        if (user) {
+            return user;
+        }
+        throw new NotFoundException();
+    }
+
+    create(body: CreateUserDto) {
+        const newUser = {
+            id: this.users[this.users.length - 1].id + 1,
+            name: body.name,
+            email: body.email,
+            age: body.age,
+        };
+        this.users.push(newUser);
+        return newUser;
+    }
+
+    update(id: string, body: UpdateUserDto) {
+        const userIndex = this.users.findIndex((user) => user.id == +id);
+        if (userIndex == -1) {
+            throw new NotFoundException();
+        }
+        this.users[userIndex].name = body.name || this.users[userIndex].name;
+        this.users[userIndex].email = body.email || this.users[userIndex].email;
+        this.users[userIndex].age = body.age || this.users[userIndex].age;
+        return this.users[userIndex];
+    }
+
+    delete(id: string) {
+        const userIndex = this.users.findIndex((user) => user.id == +id);
+        if (userIndex == -1) {
+            throw new NotFoundException();
+        }
+        return this.users.splice(userIndex, 1);
+    }
+}
