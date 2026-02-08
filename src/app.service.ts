cf59437f356ec967a87be465c9a846d6d35790e4
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-02-08 09:14:48 +0200

    feat(app): replace getHello method with getHealthCheck and update e2e tests

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return 'Hello World!';
+    getHealthCheck() {
+        return {
+            status: 'ok',
+            environment: process.env.NODE_ENV || 'development',
+            uptime: process.uptime(),
+            timestamp: new Date().toISOString(),
+            application: {
+                name: 'first-nest',
+                version: '1.0.0',
+            },
+        };
     }
 }
